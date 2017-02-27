3612a2b2ec2edd55415b01fdb05ea1b85de588bb
Daniel Fogerty <Danjfogerty@gmail.com>
2017-02-26 21:59:31 -0800

    linked list finished

--- 04. Lists/linkedList.js
+++ 04. Lists/linkedList.js
@@ -17,32 +17,85 @@ var LinkedListNode = function(data, next) {
 
 var LinkedList = function() {
   this.head = new LinkedListNode(null, null);
-  this.tail = head;
+  this.tail = this.head;
+  this.length = 0;
 };
 
-
-
 LinkedList.prototype.insert = function(index, value) {
-  // ...
+  if(index > this.length) {
+    return false;
+  }
+  if(index == 0) {
+    if(this.length == 0) {
+      this.head.data = value;
+      this.length++;
+      return true;
+    }
+    var newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+    this.length++;
+    return true;
+  }
+  var node = this.head;
+  for(var i = 0; i < index; i++) {
+    node = node.next;
+  }
+  if(index == this.length) {
+    this.length++
+    var newNode = new LinkedListNode(value, null);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return true;
+  }
+  var newNode = new LinkedListNode(value, node.next);
+  node.next = newNode;
+  this.length++;
 };
 
 LinkedList.prototype.remove = function(index) {
-  // ...
+  var node = this.head;
+  for(var i = 0; i < index-1; i++) {
+    node = node.next;
+  }
+  node.next = node.next.next;
+  this.length--;
 };
 
 LinkedList.prototype.get = function (index) {
-  // ...
+  var node = this.head;
+  for(var i = 0; i < index; i++) {
+    node = node.next;
+  }
+  return node.next;
 };
 
 LinkedList.prototype.set = function(index, value) {
-  // ...
+  var node = this.head;
+  for(var i = 0; i < index; i++) {
+    node = node.next;
+  }
+  node.data = value;
 };
 
 LinkedList.prototype.find = function(value) {
-  // ...
+  var newNode = this.head;
+  var index = this.length
+  while(index > 0) {
+    if(newNode.data == value) {
+      return newNode;
+    }
+    newNode = newNode.next;
+    index--;
+  }
+  return false;
 };
 
 LinkedList.prototype.contains = function(value) {
-  // ...
+  var node = this.head;
+  for(var i = 0; i < this.length; i++) {
+    if(node.data == value) {
+      return true;
+    }
+  }
+  return false;
 };
-

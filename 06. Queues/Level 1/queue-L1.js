387f01ee1e6dd7374332a3f88c1dcb59706a94d3
Daniel Fogerty <Danjfogerty@gmail.com>
2017-03-24 11:04:12 -0700

    did L1

--- 06. Queues/Level 1/queue-L1.js
+++ 06. Queues/Level 1/queue-L1.js
@@ -5,9 +5,9 @@
 
  [ ] Complete the implementation of a Queue class
  [x] .storage property to hold the items on the queue
- [ ] .enqueue() function to queue up a value
- [ ] .dequeue() function to dequeue a value
- [ ] .length property to return the current length
+ [x] .enqueue() function to queue up a value
+ [x] .dequeue() function to dequeue a value
+ [x] .length property to return the current length
 
  NOTE: Do not use any built-in features
  NOTE: Do not focus on edge cases or error conditions
@@ -16,14 +16,23 @@
 
 var Queue = function() {
   this.storage = [];
+  this.length = 0;
 };
 
 Queue.prototype.enqueue = function(value) {
-  // ...
+  this.storage[this.length] = value;
+  this.length++;
 };
 
 Queue.prototype.dequeue = function() {
-  // ...
+  var array = [];
+  var value = this.storage[0];
+  for(var i = 1; i < this.length; i++) {
+    array.push(this.storage[i]);
+  }
+  this.storage = array;
+  this.length--;
+  return value;
 };
 
 
